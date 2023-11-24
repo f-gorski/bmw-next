8568bd6b27d4b9a40a7837ed4a3ff817c710d15a
f-gorski <55857154+f-gorski@users.noreply.github.com>
2023-11-24 18:40:13 +0100

    feat: hero section

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image"
 import styles from "./Header.module.scss"
 import Link from "next/link"
+import bmwLogo from "./img/bmw_logo.svg"
 
 export default function Header() {
     return (
@@ -14,6 +16,7 @@ export default function Header() {
                     </li>
                 </ul>
             </nav>
+            <Image src={bmwLogo} className={styles.logo} alt="bmw logo" />
             <hr className={styles.headerLine} />
             <p className={styles.headerText}>Radość z jazdy</p>
         </header>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,33 @@
+import { useState } from "react"
+import styles from "./Hero.module.scss"
+import Image from "next/image"
+import logo from "./img/bmw_i_logo.svg"
+
 export default function Hero() {
+    const [slide, setSlide] = useState(0)
     return (
-        <>
-            <h1></h1>
-            <h2></h2>
-            <button></button>
-            <div className="heroSlider"></div>
-        </>
+        <section className={styles.heroSection}>
+            <div className={styles.slogan}>
+                <Image src={logo} className={styles.heroLogo} alt="bmw I-shaped logo" />
+                <h1 className={styles.heroHeader}>Nowe BMW iX</h1>
+                <h2 className={styles.heroSubheader}>PIONIER NOWEJ ERY.</h2>
+            </div>
+            <div className={styles.actions}>
+                <div className={styles.sliderButtons}>
+                    <div
+                        onClick={() => setSlide(0)}
+                        className={`${styles.sliderBtn} ${slide === 0 ? styles.btnActive : ""}`}></div>
+                    <div
+                        onClick={() => setSlide(1)}
+                        className={`${styles.sliderBtn} ${slide === 1 ? styles.btnActive : ""}`}></div>
+                    <div
+                        onClick={() => setSlide(2)}
+                        className={`${styles.sliderBtn} ${slide === 2 ? styles.btnActive : ""}`}></div>
+                </div>
+                <div className={styles.cta}>
+                    <button className={styles.ctaBtn}>UMÓW JAZDĘ PRÓBNĄ</button>
+                </div>
+            </div>
+        </section>
     )
 }
